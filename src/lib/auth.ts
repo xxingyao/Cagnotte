c78bf8ab4aaa2f976dd527874d8a89f6ae983b25
Lau Xing Yao <72126356+xxingyao@users.noreply.github.com>
2026-08-24 08:04:29 +0000

    Small update 2

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -7,8 +7,8 @@
 
 const API_BASE =
   process.env.NEXT_PUBLIC_API_BASE ??
-  'https://oxfhpuu8a8.execute-api.us-east-1.amazonaws.com/dev';
-
+  'https://aywlfqpys6.execute-api.us-east-1.amazonaws.com/dev';
+  
 const TOKEN_KEY = 'cagnotte.tokens';
 
 interface Tokens {
